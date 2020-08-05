8ac5924010b0e66facf55db9aff7da0c6e239ca5
Prasad Pandit <prasadp4009@gmail.com>
2020-08-04 22:44:05 -0700

    Initial Commit

---
+++ and_gate.v
@@ -0,0 +1,27 @@
+//-----------------------------------------------------------------------------
+// GcdUnit
+//-----------------------------------------------------------------------------
+//
+// Originally Generated from PyMTL with a few modifications to make it more
+// friendly to OpenROAD tools
+//
+// dump-vcd: False
+// verilator-xinit: zeros
+//module and_gate
+module and_gate
+(
+  input  wire clk,
+  input  wire reset,
+  input  wire a,
+  input  wire b,
+  output reg  y
+);
+
+always @(posedge clk or posedge reset) begin
+  if (reset) begin
+    y <= 0;
+  end
+  else begin
+      y <= a & b;
+  end
+end
